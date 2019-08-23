1b4d4f873cc959ddf987c1d36c6588d022260cfd
Luis Amaya <luis.amayaalvarenga@gmail.com>
2019-08-22 21:17:11 -0600

    Nuevo repo

---
+++ ConsoleAppPractice/ConsoleAppPractice/General.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPractice
+{
+    //Clase representativa a los casos a resolver de Hacker Rank
+    public class General
+    {
+
+    }
+}
