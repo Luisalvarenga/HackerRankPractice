a3a9d9c990866f6122ae964b76139aa888dac1b1
Luis Alvarenga <luis.amayaalvarenga@gmail.com>
2021-05-05 22:59:10 -0600

    Redo of clases and organize levels

---
+++ ConsoleAppPractice/ConsoleAppPractice/Easy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPractice
+{
+    public class Easy
+    {
+        public List<int> breakingTheRecords(List<int> scores)
+        {
+            int counterMax = 0, counterMin = 0, max = scores[0], min = scores[0];
+            List<int> results = new List<int>();
+
+            foreach (var item in scores)
+            {
+                if (item > max)
+                {
+                    max = item;
+                    counterMax++;
+                }
+                if (item < min)
+                {
+                    min = item;
+                    counterMin++;
+                }
+
+            }
+            results.Add(counterMax);
+            results.Add(counterMin);
+            return results;
+        }
+
+
+    }
+}
--- ConsoleAppPractice/ConsoleAppPractice/Program.cs
+++ ConsoleAppPractice/ConsoleAppPractice/Program.cs
@@ -1,8 +1,16 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
 using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace ConsoleAppPractice
 {
@@ -10,6 +18,17 @@ namespace ConsoleAppPractice
     {
         static void Main(string[] args)
         {
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> scores = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(scoresTemp => Convert.ToInt32(scoresTemp)).ToList();
+
+            Easy e = new Easy();
+            List<int> result = e.breakingTheRecords(scores);
+
+            Console.WriteLine(String.Join(" ", result));
+
+            Console.Read();
         }
     }
 }
